c602b1e0e7f12b5565f7c34c75dca6752a8d07a8
walruii <walruiix@gmail.com>
2023-07-06 17:51:07 +0530

    worked on the weather app fixed the signup website and removed things that are not needed

--- web/javascript/newsletter/app.js
+++ web/javascript/newsletter/app.js
@@ -4,8 +4,15 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static("public"));
+
 app.get("/", function (req, res) {
     res.sendFile(__dirname + "/signup.html");
+})
+
+app.post("/", function (req, res) {
+    var firstName = req.body.fName
+    var lastName = req.body.lName
+    var email = req.body.email
 })
 
 app.listen(8000, function () {
