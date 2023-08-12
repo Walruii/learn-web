574ea3e014072acb51bd3563b5fca137d94cda35
walruii <walruiix@gmail.com>
2023-08-13 01:14:51 +0530

    updating nth check and fixed env formatting

--- web/javascript/react/secrets/middleware/mongooseconnect.js
+++ web/javascript/react/secrets/middleware/mongooseconnect.js
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose';
 
-const connectMongo = async () => mongoose.connect(process.env.MONGOURI);
+const connectMongo = async () => mongoose.connect(process.env.MONGO_URI);
 
 export default connectMongo;
